91f1b5601028f62bbc327bd395581c8c602e2c67
jingyang.kwek <jingyang.kwek@bytedance.com>
2025-01-26 20:59:37 +0800

    feat: use polar to read parquet schema

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 #![warn(unused_extern_crates)]
+use polars::prelude::*;
 
-// TODO: add so many mods here?
-// good rust dir structure?
 mod constant;
 mod graph;
 fn main() {
-    graph::vamana::init();
+    // graph::vamana::init();
+    let res = read_dataset(
+        "dataset/dbpedia-entities-openai-1M/train-00000-of-00026-3c7b99d1c7eda36e.parquet",
+    );
+    println!("{:?}", res);
+}
+
+fn read_dataset(dataset_file: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let args = ScanArgsParquet::default();
+    let lf = LazyFrame::scan_parquet(dataset_file, args).unwrap();
+
+    let schema = lf.schema().unwrap();
+    print!("{:?}", schema);
+
+    Ok(())
 }
